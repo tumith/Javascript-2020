2daee44e086ebf89868c6f6cf8662173dc948d2b
tumith <34027928+tumith@users.noreply.github.com>
2020-03-04 18:40:49 +0000

    Add files via upload

--- verkefni 1/app.js
+++ verkefni 1/app.js
@@ -3,99 +3,168 @@ const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight
+const height = canvas.height = window.innerHeight;
+const img = document.getElementById('Image');
+
+//--------------------------------------
+function getMousePosition(event) {
+    let rect = canvas.getBoundingClientRect(); 
+    let xn = event.clientX - rect.left; 
+    let yn = event.clientY - rect.top;
+    return [xn, yn];
+}
+
+const pos = canvas.addEventListener("mousedown", event => {
+    boxs[0].movingRect(event);
+});
+//--------------------------------------
 
 function random(min, max) {
     const num = Math.floor(Math.random() * (max - min + 1)) + min;
     return num;
 }
 
+
 class Ball {
-    constructor(x, y, velX, velY, color, size) {
-        this.x = x;
-        this.y = y;
+    constructor(x, y,velX, velY, color) {
+        this.x = 580;
+        this.y = 250;
         this.velX = velX;
         this.velY = velY;
         this.color = color;
-        this.size = size;
+        this.width = 150;
+        this.height = 150;
     }
     
     collisionDetect() {
-        for (let j = 0; j < balls.length; j++) {
-            if (!(this === balls[j])) {
-                const dx = this.x - balls[j].x;
-                const dy = this.y - balls[j].y;
+        let sizeN = this.width * this.height;
+        for (let j = 0; j < alls.length; j++) {
+            if (!(this === alls[j])) {
+                const dx = this.x - alls[j].x;
+                const dy = this.y - alls[j].y;
                 const distance = Math.sqrt(dx * dx + dy * dy);
                 
-                if (distance < this.size + balls[j].size) {
-                    balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+                if (distance < sizeN + alls[j].sizeN) {
+                    this.velX = 0;
+                    this.velY = 0;
                 }
             }
         }
     }
     
     update() {
-        if ((this.x + this.size) >= width) {
+        if ((this.x + this.width) >= width) {
+            this.velX = -(this.velX);
+        }
+        if ((this.x) <= 0) {
             this.velX = -(this.velX);
         }
     
-        if ((this.x - this.size) <= 0) {
-            this.velX = -(this.velX);
+        if ((this.y + this.height) >= height) {
+            this.velY = -(this.velY);
         }
-        
-        if ((this.y + this.size) >= height) {
+        if ((this.y) <= 0) {
             this.velY = -(this.velY);
         }
         
-        if ((this.y - this.size) <= 0) {
-            this.velY = -(this.velY);
-        }
-        
+
         this.x += this.velX;
         this.y += this.velY;
     }
     
     draw() {
-        // ctx.fillStyle = this.color;
-        // ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-        // ctx.fill();
-        // ctx.beginPath();
-        let img = new Image();
-        img.onload = function() {
-            ctx.drawImage(img,this.x, this.y);
-        }
+        ctx.drawImage(img, this.x, this.y,this.width,this.height);
         img.src = 'https://tskoli.is/wp-content/uploads/2017/11/Gunnar-Thorunnarson-150x150.jpg';
-    } 
+    }
 }
 
-let balls = [];
+class Box {
+    constructor(x, y,velX, velY, color) {
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+        this.color = color;
+        this.width = 150;
+        this.height = 150;
+    }
+    
+    collisionDetect() {
+        let sizeN = this.width * this.height;
+        for (let j = 0; j < boxs.length; j++) {
+            if (!(this === boxs[j])) {
+                const dy = this.y - boxs[j].y;
+                const dx = this.x - boxs[j].x;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+                
+                if (distance < sizeN + boxs[j].sizeN) {
+                    this.velX = 0;
+                    this.velY = 0;
+                }
+            }
+        }
+    }
+    
+    drawRect() {
+        ctx.beginPath();
+        ctx.fillStyle = 'rgba('+ random(0,255) +','+ random(0,255) +','+ random(0,255) +','+ random(0,255) +')';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
 
-while (balls.length < 25) {
-    let size = random(10,20);
+    movingRect(event){
+        let [x, y] = getMousePosition(event);
+        this.x = x;
+        this.y = y;
+        console.log(x,y);
+    }
+}
+
+let alls = [];
+let boxs = [];
+
+while (alls.length < 25) {
     let ball = new Ball(
         // ball position always drawn at least one ball width
         // away from the edge of the canvas, to avoid drawing errors
-        random(0 + size,width - size),
-        random(0 + size,height - size),
+        random(0,height),
+        random(0,width),
         random(-7,7),
         random(-7,7),
-        'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
-        size
+        'rgba(' + random(0, 3) + ',' + random(0, 3) + ',' + random(0, 3) + ', 0.2)'
     );
 
-    balls.push(ball);
+    alls.push(ball);
+}
+
+while (boxs.length < 1) {
+    let box = new Box(
+        // ball position always drawn at least one ball width
+        // away from the edge of the canvas, to avoid drawing errors
+        random(0,height),
+        random(0,width),
+        random(-7,7),
+        random(-7,7),
+        'rgba(' + random(0, 3) + ',' + random(0, 3) + ',' + random(0, 3) + ', 0.2)'
+    );
+    
+    boxs.push(box);
 }
 
 function loop() {
     ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
     ctx.fillRect(0, 0, width, height);
     
-    for (let i = 0; i < balls.length; i++) {
-        balls[i].draw();
-        balls[i].update();
-        balls[i].collisionDetect();
+    for (let i = 0; i < alls.length; i++) {
+        alls[i].draw();
+        alls[i].update();
+        alls[i].collisionDetect();
     }
     
+    for (let i = 0; i < boxs.length; i++) {
+        boxs[i].drawRect();
+        boxs[i].collisionDetect();
+    }
+
     requestAnimationFrame(loop);
 }
 
